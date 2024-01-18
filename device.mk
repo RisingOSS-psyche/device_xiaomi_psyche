9285cce3228c8dd2d1565a9f8f6b08a787a6e8a3
johnmart19 <johnivan19999@gmail.com>
2024-01-18 22:03:20 +0200

    psyche: Patch Dolby Libs to use fallback to VNDK33 stagefright_foundation
    - VNDK34 part was updated and our service requires to have fallback in order to work appropriately
    - Copy Respectfully arm and arm64 libs

--- device.mk
+++ device.mk
@@ -33,6 +33,11 @@ PRODUCT_COPY_FILES += \
 # Dolby Support
 TARGET_USES_MIUI_DOLBY := true
 
+# Dolby VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33-dolby.so \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33-dolby.so
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
