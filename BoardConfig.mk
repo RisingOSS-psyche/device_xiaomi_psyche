6efe62e2d0016739305a2d8bca0578378ed4c276
Flicker372 <flicker372@outlook.com>
2024-02-21 14:24:43 +0800

    psyche: Import Xiaomi fx tunnel manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,9 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Xiaomi Manifest
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/xiaomi_manifest.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_psyche
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_psyche
