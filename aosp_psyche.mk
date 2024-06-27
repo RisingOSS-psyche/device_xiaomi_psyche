72d5cb96efa9acab597d0ecc09821a2dbab6825a
NotZeetaa <rodrigo2005contente@gmail.com>
2024-06-27 08:40:15 +0000

    psyche: Minimal PixelOS Bringup

--- aosp_psyche.mk
+++ aosp_psyche.mk
@@ -8,13 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOs stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Device Specific Flags
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Environment Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 # Inherit from psyche device
 $(call inherit-product, device/xiaomi/psyche/device.mk)
 
-PRODUCT_NAME := lineage_psyche
+PRODUCT_NAME := aosp_psyche
 PRODUCT_DEVICE := psyche
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
