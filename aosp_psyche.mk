cc0218634a4e66aac7d1c856380978a8964f54d9
Flicker372 <flicker372@outlook.com>
2024-07-21 01:46:43 +0800

    psyche: Update blobs from V816.0.6.0.TLDCNXM * HotwordEnrollment from psyche_global-user 13 TKQ1.221114.001 V816.0.3.0.TLDMIXM release-keys

--- aosp_psyche.mk
+++ aosp_psyche.mk
@@ -33,4 +33,4 @@ PRODUCT_MODEL := Xiaomi 12X
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/psyche/psyche:13/TKQ1.221114.001/V816.0.3.0.TLDCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/psyche/psyche:13/TKQ1.221114.001/V816.0.6.0.TLDCNXM:user/release-keys
